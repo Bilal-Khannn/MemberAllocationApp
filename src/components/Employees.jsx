2b98125d9cc33a788947da7a5217cbc204432744
Bilal <bkhan8423@gmail.com>
2023-02-18 12:21:27 +0000

    first commit

---
+++ src/components/Employees.jsx
@@ -0,0 +1,69 @@
+import femaleProfile from "/home/bk/vscode-projects/project-01/src/images/femaleProfile.jpg";
+import maleProfile from "/home/bk/vscode-projects/project-01/src/images/maleProfile.jpg";
+
+function Employees({
+  employees,
+  selectedTeam,
+  handleEmployeeCardClick,
+  handleTeamSelectionChange,
+}) {
+  return (
+    <main className="container">
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-8">
+          <select
+            className="form-select form-select-lg"
+            value={selectedTeam}
+            onChange={handleTeamSelectionChange}
+          >
+            <option value="TeamA">TeamA</option>
+            <option value="TeamB">TeamB</option>
+            <option value="TeamC">TeamC</option>
+            <option value="TeamD">TeamD</option>
+          </select>
+        </div>
+      </div>
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-8">
+          <div className="card-collection">
+            {employees.map((employee) => (
+              <div
+                key={employee.id}
+                id={employee.id}
+                className={
+                  employee.teamName === selectedTeam
+                    ? "card m-2"
+                    : "standout m-2"
+                }
+                style={{ cursor: "pointer" }}
+                onClick={handleEmployeeCardClick}
+              >
+                {employee.gender === "female" ? (
+                  <img
+                    src={femaleProfile}
+                    alt="female dummy"
+                    className="card-img-top"
+                  />
+                ) : (
+                  <img
+                    src={maleProfile}
+                    alt="male dummy"
+                    className="card-img-top"
+                  />
+                )}
+                <div className="card-body">
+                  <h5 className="card-title">Full Name: {employee.fullName}</h5>
+                  <p className="card-text">
+                    <b>Designation:</b> {employee.designation}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Employees;
